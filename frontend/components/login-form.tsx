341f88609d718d61da37c45e34f571a91d0938f8
likeca <like.ca@gmail.com>
2026-07-13 17:47:45 -0400

    remove recaptcha policy

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -415,19 +415,6 @@ export function LoginForm({ className, recaptchaSiteKey = '', ...props }: LoginF
         {', '}
         and <a href="/privacy">Privacy Policy</a>.
       </FieldDescription>
-      {recaptchaSiteKey && (
-        <FieldDescription className="px-6 text-center text-xs">
-          This site is protected by reCAPTCHA and the Google{' '}
-          <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer">
-            Privacy Policy
-          </a>{' '}
-          and{' '}
-          <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">
-            Terms of Service
-          </a>{' '}
-          apply.
-        </FieldDescription>
-      )}
     </div>
   );
 }
